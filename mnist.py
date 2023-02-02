f86af6441f02cdfe4be15aeab7e2819ae6c0f1c3
gr4bherr <gr4bherr@fastmail.com>
2023-02-02 15:12:34 +0100

    cleanup

--- mnist.py
+++ mnist.py
@@ -61,9 +61,10 @@ def softmax(inputs):
 def layer(x, w, b):
   return np.dot(x, w) + b
 
-def loss(x,y):
+# categorical cross entropy loss
+def loss(o, l):
   # todo: clip
-  confidence = np.sum(x*y, axis=1)
+  confidence = np.sum(o * l, axis=1)
   neglog = -np.log(confidence)
   return np.mean(neglog) 
 
@@ -90,89 +91,9 @@ print("loss:", loss(a2, labels))
 
 print("accuracy", accuracy(a2, labels))
 
-print(a2[0])
-print(labels[0])
-dz2 = a2 - labels
-dw2 = 1 / m * dz2.dot(a1.t)
-print(dz2[0])
-print(dw2[0])
 
 
 
 
 
 
-
-# def sigmoid(X):
-#   return 1 / (1 + np.exp(-X))
-
-# def sigmoidDerivative(X):
-#   return sigmoid(X) * (1 - sigmoid(X))
-
-# def correctness(O, L):
-#   c = 0
-#   #for i in range(imgNum):
-#   for i in range(1):
-#     c += np.argmax(O) == np.argmax(L)
-#   return c
-
-
-#for i in range(imgNum):
-#for i in range(1):
-  #L = labels[:, 0]
-  #I = images[: ,0]
-
-  # init
-  #W1 = np.random.rand(16, 784) - 0.5
-  #b1 = np.random.rand(16,) - 0.5 # todo: maybe initialize as 0
-  #W2 = np.random.rand(10, 16) - 0.5
-  #b2 = np.random.rand(10,) - 0.5
-
-  # forward prop
-  #H = sigmoid(W1 @ I + b1) # todo: make H small
-  #O = sigmoid(W2 @ H + b2)
-
-  # back prop
-
-
-  # 2/n  *  (O - L)  # some kind of derivative
-
-  #E = (O - L) ** 2
-  #E = (1 / 10) * (np.sum(((O - L) ** 2), axis = 0)) # mean squared error
-  #LR = 0.01
-
-  #delta2 = O - L
-  #print(delta2)
-  #print(O)
-  #W2 = -0.01 * (delta2 @ O.T)
-  #print(W2)
-  #print(W2.shape)
-
-
-
-
-
-
-  
-
-
-
-# # FORWARD PROPAGATION
-# # feature extracotr
-# W1 = np.random.rand(10, 784) - 0.5
-# b1 = np.random.rand(10, 1) - 0.5
-# A1 = sigmoid(W1 @ images + b1)
-# # classifier
-# W2 = np.random.rand(10, 10) - 0.5
-# b2 = np.random.rand(10, 1) - 0.5
-# A2 = sigmoid(W2 @ A1 + b2)
-
-# # cost function
-# e = meanSquaredError(A2, labels)
-# print(correctness(A2, labels))
-
-# D2 = A2 - labels
-# W2 = -LR * D2 @ A1.T
-# b2 = -LR * D2
-# print(W2)
-# print(b2)
